d1cd0fdd394a2e2458fcf0ae9567353903c67b50
KD4-RahulYadav-83582 <rahulyadav0366@gmail.com>
2024-08-10 20:30:20 +0530

    Backend Template Uploaded

---
+++ Backend/AFPAS/Models/UNITS.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AFPAS.Models
+{
+    public class UNITS
+    {
+        [Key]
+        public int LOC_ID { get; set; }
+
+        [StringLength(50)]
+        public string LOC_NAME { get; set; }
+
+      
+        public int LOC_POINTS { get; set; }
+    }
+
+}
